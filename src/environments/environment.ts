debc4d476cdfa497bc6b2c7cb29d70c183d9d886
Juan Camilo Neira Campos <j.neira@uniandes.edu.co>
2023-03-12 00:34:36 -0500

    update

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,7 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-   production: true,
+   production: false,
    name: "DEV",
    //endpoint: "https://octaedro-backend.alephmind.co",
    endpoint: "https://octaedro-api.sergiomolina.me",
