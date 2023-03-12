f6b10404c41ff274531747ac12e00cf4e932a490
Juan Camilo Neira Campos <j.neira@uniandes.edu.co>
2023-03-12 18:09:20 -0400

    update

--- server.js
+++ server.js
@@ -1,12 +1,11 @@
 const express = require('express');
-const path = require('path');
 const app = express();
+const path = require('path');
 
-app.use(express.static(__dirname + '/dist/frontend'));
+app.use(express.static('./dist/frontend'));
 
-app.get('/*', function(req,res) {
-
-res.sendFile(path.join(__dirname+'/dist/frontend/index.html'));
-});
+app.get('/*', (req, res) =>
+    res.sendFile('index.html', {root: 'dist/frontend/'}),
+);
 
 app.listen(process.env.PORT || 8080);
