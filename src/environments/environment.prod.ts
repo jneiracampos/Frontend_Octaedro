f905030715fd45f12df6d61e12edd4fe2c31e1e2
Juan Camilo Neira Campos <j.neira@uniandes.edu.co>
2023-04-18 21:52:48 -0400

    enviroment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,8 +1,8 @@
 export const environment = {
-  production: true, 
-  name: "PROD", 
+  production: true,
+  name: "PROD",
   //endpoint: "https://octaedro-backend.alepihmnd.co",
-  endpoint: "https://octaedro.com.co",
+  endpoint: "https://octaedro.sergiomolina.me",
   //recaptchaCode: "6LcbLdkaAAAAAL3tdGUtvfVprH5VEhH7hnB0jffX"
   recaptchaCode: "6Ld92wcbAAAAAPek4Y8q4YOj-CPJ4dG8ojHymDRs",
   version: "0.0.5"
